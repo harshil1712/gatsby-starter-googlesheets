6fd9f070f2ce0802a4efc46a06245b5b3d06f942
Harshil Agrawal <ghagrawal17@gmail.com>
2019-05-22 15:56:26 +0530

    added CSS to cards

--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,17 @@
 import React from "react"
 
 const Card = (props) =>(
-    <div>
+    <div
+        style={{
+            border:'1px solid #eaeaea',
+            boxShadow:'2px 2px 5px grey',
+            padding:'10px',
+            marginBottom:'10px',
+            borderRadius:'10px'
+        }}
+    >
         <h1>{props.title}</h1>
-        <p>{props.date}</p>
+        <p style={{color:'grey'}}>{props.date}</p>
         <p>{props.content}</p>
     </div>
 )
