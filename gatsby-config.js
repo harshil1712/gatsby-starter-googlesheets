89de261544728258fa5f426a266507dda1fd328a
Harshil Agrawal <ghagrawal17@gmail.com>
2019-08-06 06:24:01 +0530

    updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,19 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+const buildCredentials = ({ PROJECT_ID, PRIVATE_KEY, PRIVATE_KEY_ID }) => ({
+  type: 'service_account',
+  project_id: PROJECT_ID,
+  private_key_id: PRIVATE_KEY_ID,
+  private_key: PRIVATE_KEY.replace(/(\\r)|(\\n)/g, '\n'),
+  client_email: `fetchdata@${PROJECT_ID}.appspot.gserviceaccount.com`,
+  client_id: '',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/fetchdata%40${PROJECT_ID}.appspot.gserviceaccount.com`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
